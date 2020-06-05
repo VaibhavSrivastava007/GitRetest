dd9c9a928e1e1dd8c0d473c7e8b4ea24d9528592
vaibhav <30vaibhav@gmail.com>
2020-06-05 14:08:02 +0530

    commit first code retestone

---
+++ src/test/java/GitTestOne_Google.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+
+public class GitTestOne_Google {
+
+	
+	@Test
+	public void Gitonecreategoogle() {System.out.println("print Gitonecreategoogle");}
+	
+	@Test
+	public void Gitonecreategmail() {System.out.println("print Gitonecreategmail");}
+	
+	@Test
+	public void Gitonedeletegoogle() {System.out.println("print Gitonedeletegoogle");}
+	
+	
+	
+}
---
+++ src/test/java/GitTestOne_Youtube.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+public class GitTestOne_Youtube {
+
+	
+	@Test
+	public void GitoneYoutubecreate() {System.out.println("print GitoneYoutubecreate");}
+	
+	@Test
+	public void GitoneYoutubedelete() {System.out.println("print GitoneYoutubedelete");}
+	
+	@Test
+	public void GitoneYoutubeenabled() {System.out.println("print Gitonegoogle");}
+	
+	
+	
+	
+	
+}
